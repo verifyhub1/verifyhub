49ad2a3c96017061da205722002d5b7d7d258559
Alexplayrus1 <118437098+Alexplayrus2@users.noreply.github.com>
2022-11-26 16:02:14 +0300

    Create headcraphead.lua

---
+++ headcraphead.lua
@@ -0,0 +1,6 @@
+--headcrap.
+char=game.Players.LocalPlayer.Character
+char.Necklace.Handle.Mesh:Destroy()
+char.RobloxVisor.Handle.Mesh:Destroy()
+char.PlaidWrapHat.Handle.Mesh:Destroy()
+char.Head.Mesh:Destroy()
