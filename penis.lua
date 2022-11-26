0a19859b0c63de8df79185bce8998a6ccb65c8cf
Alexplayrus1 <118437098+Alexplayrus2@users.noreply.github.com>
2022-11-26 17:12:53 +0300

    Create penis.lua

---
+++ penis.lua
@@ -0,0 +1,25 @@
+local char=game.Players.LocalPlayer.Character
+local mouse=game.Players.LocalPlayer:GetMouse()
+local plr=game.Players.LocalPlayer
+local hand=char.Necklace.Handle
+local runservice=game:GetService("RunService")
+local cfval=Instance.new("CFrameValue")
+local ts=game:GetService("TweenService")
+hand.Mesh:Destroy()
+cfval.Changed:Connect(function()
+char:SetPrimaryPartCFrame(cfval.Value)
+end)
+mouse.Button1Down:Connect(function()
+if mouse.Target.Parent:FindFirstChildOfClass("Humanoid") then
+local targetroot=mouse.Target.Parent.HumanoidRootPart
+cfval.Value=char.HumanoidRootPart.CFrame
+hold = runservice.RenderStepped:Connect(function()
+char:SetPrimaryPartCFrame(targetroot.CFrame + Vector3.new(0,-2,0))
+end)
+end
+end)
+mouse.Button1Up:Connect(function()
+if hold then
+hold:Disconnect()
+end
+end)
